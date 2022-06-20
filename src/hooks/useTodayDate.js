d8c2b25ed663574f868a5b6b23ab49effd693d92
rodolphe37 <rodolphe.a@gmail.com>
2022-06-20 09:15:23 +0200

    added universal date format instead of french format

--- src/hooks/useTodayDate.js
+++ src/hooks/useTodayDate.js
@@ -1,14 +1,8 @@
 const useTodayDate = () => {
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
   const today = new Date();
   const todayMls = Math.round(new Date() / 1000);
 
-  const LocalDate = today.toLocaleDateString("fr-FR", options);
+  const LocalDate = today.toLocaleDateString();
 
   return {
     LocalDate,
